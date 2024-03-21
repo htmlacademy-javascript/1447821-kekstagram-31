46f9764f60eb39bc99192ceb5a623a565d27783d
Данил Глазунов <danil_glazunov_2100@mail.ru>
2024-03-21 19:37:32 +0300

    4 задание

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 function result (string, maxSymbols){
   return (string.length <= maxSymbols) ? 'true' : 'false';
 }
 
+// eslint-disable-next-line no-unused-vars
 function comparesStringLength (string, length) {
   if (string.length <= length){
     return true;
   } else {
-  return false;
+    return false;
   }
 }
